4cdff3b7eccdfae49e3a9b2a3ab766717aebf61d
Abramov Ivan <airvan21@gmail.com>
2013-10-26 21:30:51 +0400

    fixed error counter

--- HW4/lagrange.m
+++ HW4/lagrange.m
@@ -2,7 +2,7 @@ classdef lagrange
     
     properties
         % Number of parts, on which will divide interval
-        nPart = 20;
+        nPart = 25;
         % Nodes for interpolation
         nodes
         % Current function for interpolation
@@ -24,30 +24,34 @@ classdef lagrange
         % Constructor
         % inputNode - interpolation nodes
         function obj = lagrange(func, curInterval, inputNode)
-            obj.nodes = inputNode; 
+            obj.nodes = cell(obj.testAmount);
+            obj.nodes{1} = inputNode;
             obj.intFunc = func;
             obj.interval = curInterval;
-            obj.lagPol = obj.setLagPol();
+            [obj.lagPol, obj.nodes] = obj.setLagPol();
             obj.aMax = obj.calcMax();
             obj.output();
-            % Add error calc for all + File Output
+            obj.buildAllPlot();
+            % Add File Output
         end
         
         % Building plot for all
+        % Rewrite it
         function buildAllPlot(obj)
             figure;
             ezplot(obj.intFunc, [obj.interval(1) obj.interval(2)]);
             hold all
+            title('All plots');
             MatCus = obj.fillMat(1);
             plot(MatCus(:, 1), MatCus(:,3), 'Color', 'r');
             MatLeft = obj.fillMat(2);
-            %plot(MatLeft(:, 1), MatLeft(:,3), 'Color', 'g');
+            plot(MatLeft(:, 1), MatLeft(:,3), 'Color', 'g');
             MatMiddle = obj.fillMat(3);
-            %plot(MatMiddle(:, 1), MatMiddle(:,3), 'Color', 'g');
+            plot(MatMiddle(:, 1), MatMiddle(:,3), 'Color', 'y');
             MatRight = obj.fillMat(4);
-            %plot(MatRight(:, 1), MatRight(:,3), 'Color', 'r');
+            plot(MatRight(:, 1), MatRight(:,3), 'Color', 'b');
             MatFull = obj.fillMat(5);
-            %plot(MatFull(:, 1), MatFull(:,3), 'Color', 'g');
+            plot(MatFull(:, 1), MatFull(:,3), 'Color', 'black');
             hold off
         end
         
@@ -93,7 +97,7 @@ classdef lagrange
                 % |f(x) - PLagrange(x)|
                 matrix(i, 4) = abs(matrix(i, 2) - matrix(i, 3));
                 % A - Error
-                matrix(i, 5) = obj.calcError(obj.nodes, arg);
+                matrix(i, 5) = obj.calcError(obj.nodes{index}, arg);
             end
         end
         
@@ -108,6 +112,7 @@ classdef lagrange
         end
         
         % Calculates derivation maximum on interval
+        % Has problems with min in cos
         function maxValue = calcMax(obj)
             syms x;
             % Calculates derivation
@@ -115,29 +120,31 @@ classdef lagrange
             % Finding argument for maximum value on interval
             maxArg = fminbnd(devFunction, obj.interval(1), obj.interval(2));
             % Calculates maximum value abs
-            maxValue = abs(subs(diff(obj.intFunc, length(obj.nodes)), x, maxArg));
+            maxValue = abs(subs(diff(obj.intFunc, length(obj.nodes{1})), x, maxArg));
         end 
         
         % Create Lagrange polynoms for 'testAmount' cases 
-        function polynoms = setLagPol(obj)
+        function [polynoms, nodes] = setLagPol(obj)
+            nodes = cell(obj.testAmount);
             % Polynom on input data
-            polynoms(1) = obj.initPol(obj.nodes);
+            polynoms(1) = obj.initPol(obj.nodes{1});
+            nodes{1} = obj.nodes{1};
             % Nodes in a left, middle, right part of an interval 
-            len = length(obj.nodes);
-            leftNodes = zeros(1, len);
-            rightNodes = zeros(1, len);
-            middleNodes = zeros(1, len);
-            % Could be rewritten using rand()
+            len = length(obj.nodes{1});
             state = obj.interval(1);
+            % 1/2 Intreval length
+            nodeArea = (obj.interval(2) - obj.interval(1)) / 2;
+            % 1/8 Interval length
             step = (obj.interval(2) - obj.interval(1)) / (2 * len);
-            for i = 1:len
-                leftNodes(i) = state;
-                middleNodes(i) = state + (len / 2) * step;
-                rightNodes(i) = state + len * step;
-                state = state + step;
-            end
+            leftNodes = state + rand(1, len) * nodeArea;
+            rightNodes = state + 4*step +  rand(1, len) * nodeArea;
+            middleNodes = state + 2*step +  rand(1, len) * nodeArea;
             % Double amount of nodes
-            doubleNodes = obj.interval(1) + rand(1,2 * len) * (obj.interval(2) - obj.interval(1));
+            doubleNodes = obj.interval(1) + rand(1,2 * len) * 2 * nodeArea;
+            nodes{2} = leftNodes;
+            nodes{3} = middleNodes;
+            nodes{4} = rightNodes;
+            nodes{5} = doubleNodes;
             % Create appropriate polynoms
             polynoms(2) = obj.initPol(leftNodes);
             polynoms(3) = obj.initPol(middleNodes);
@@ -147,6 +154,12 @@ classdef lagrange
         
         % Build interpolation-plots for all cases
         function output(obj)
+            titles = cell(obj.testAmount);
+            titles{1} = 'Input data';
+            titles{2} = 'Left side nodes';
+            titles{3} = 'Middle side nodes';
+            titles{4} = 'Right side nodes';
+            titles{5} = 'Double precision';
             for i = 1:obj.testAmount
                 figure;
                 ezplot(obj.intFunc, [obj.interval(1) obj.interval(2)]);
@@ -155,6 +168,7 @@ classdef lagrange
                 % Take arguments and Lagrange polynom value
                 Mat = obj.fillMat(i);
                 plot(Mat(:, 1), Mat(:,3), 'Color', 'r');
+                title(titles{i});
                 hold off
             end
         end
